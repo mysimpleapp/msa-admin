73ec5418f1c14070fdee6de73e343e5f3a800884
Nicolas CARREZ <ncarrez@csod.com>
2020-05-10 11:23:00 +0200

    impl startMsaModule

--- index.js
+++ index.js
@@ -1,62 +1,84 @@
-const msaAdmin = module.exports = new Msa.Module()
-
-const { Perm } = Msa.require("user/perm")
+const { Perm, permAdmin } = Msa.require("user/perm")
 
 // register ///////////
 
 const AdminPanels = []
+const AdminPanelsRouter = Msa.express.Router()
 
 class AdminPanel {
-	constructor(kwargs){
-		for(let k of ["route", "app", "title"])
-			if(kwargs[k] === undefined)
+	constructor(kwargs) {
+		for (let k of ["route", "app", "title"])
+			if (kwargs[k] === undefined)
 				throw `Missing arg "${k}"`
 		Object.assign(this, kwargs)
 	}
 }
 const AdminPanelPt = AdminPanel.prototype
 
-AdminPanelPt.perm = new Perm({ group:"admin" })
+AdminPanelPt.perm = permAdmin
 
-msaAdmin.register = function(kwargs) {
+function registerAdminPanel(kwargs) {
 	const panel = new AdminPanel(kwargs)
 	AdminPanels.push(panel)
 	const mdw = Msa.express.Router()
-	msaAdmin.app.use(panel.route, mdw)
+	AdminPanelsRouter.use(panel.route, mdw)
 	mdw.use(panel.perm.checkPage())
 		.use(panel.app)
 }
 
 // register shell
-msaAdmin.register({
+registerAdminPanel({
 	route: '/sh',
 	app: require('./sh').app,
 	title: "Shell",
 	help: "Shell panel"
 })
 
-// admin app ////////////////////:
+// admin module
 
-const userMdw = Msa.require("user/mdw")
-const { unauthHtml } = Msa.require("user")
+class MsaAdminModule extends Msa.Module {
 
-msaAdmin.app.use(userMdw)
-
-// root get
-msaAdmin.app.get('/', (req, res, next) => {
-	const authPanels = AdminPanels.filter(p => p.perm.check(req.session.user))
-	if(authPanels.length === 0)
-		res.sendPage(unauthHtml)
-	else {
-		let html = "<ul>"
-		authPanels.forEach(p => {
-			const href = Msa.joinUrl(req.baseUrl, p.route)
-			html += `<li><a href="${href}">${p.title}</a></li>`
-		})
-		html += "</ul>"
-		res.sendPage(html)
+	constructor(...args) {
+		super(...args)
+		this.initApp()
 	}
-})
+
+	initApp() {
+		// user
+		this.app.use(userMdw)
+
+		// panels
+		this.app.use(AdminPanelsRouter)
+
+		// root get
+		this.app.get('/', (req, res, next) => {
+			const authPanels = AdminPanels.filter(p => p.perm.check(req.session.user))
+			if (authPanels.length === 0) {
+				res.sendPage(unauthHtml)
+			} else {
+				let html = "<ul>"
+				authPanels.forEach(p => {
+					const href = joinUrl(req.baseUrl, p.route)
+					html += `<li><a href="${href}">${p.title}</a></li>`
+				})
+				html += "</ul>"
+				res.sendPage(html)
+			}
+		})
+	}
+}
+
+// export
+
+module.exports = {
+	startMsaModule: () => new MsaAdminModule(),
+	MsaAdminModule,
+	registerAdminPanel
+}
+
+// import
+
+const { unauthHtml, userMdw } = Msa.require("user")
 
 // utils
 
